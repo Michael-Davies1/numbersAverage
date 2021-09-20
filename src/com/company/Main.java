12d6774f4e0f1367fa1c604dc3ff87a4bbe5232c
Michael Davies <MD210515@reigate.ac.uk>
2021-09-20 10:01:06 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] Numbers = new int [5];
+        int count = 0
+                while (count < 5) {
+                    System.out.println("please enter a number");
+                    Numbers[] = input.nextInt()
+                }
+                if (count == 5)  {
+
+    }
+}
